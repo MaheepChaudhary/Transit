2a7a07d88ba55ed9a03427e198dcb7864b8dd1f1
MaheepChaudhary <chaudhary.maheep28@gmail.com>
2024-09-19 22:00:22 +0530

    run file setup

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
 from imports import *
 from dataset import *
 
-def model_selection(model_name):
+def model_selection(model_name, device):
     if model_name == "Pythia14m":
-        model = LanguageModel("EleutherAI/pythia-14m", device_map="cpu")
+        model = LanguageModel("EleutherAI/pythia-14m", device_map=device)
     elif model_name == "Pythia70m":
-        model = LanguageModel("EleutherAI/pythia-70m", device_map="cpu")
+        model = LanguageModel("EleutherAI/pythia-70m", device_map=device)
     elif model_name == "Pythia160m":
-        model = LanguageModel("EleutherAI/pythia-70m", device_map="cpu")
+        model = LanguageModel("EleutherAI/pythia-70m", device_map=device)
     elif model_name == "Pythia410m":
-        model = LanguageModel("EleutherAI/pythia-70m", device_map="cpu")
+        model = LanguageModel("EleutherAI/pythia-70m", device_map=device)
     elif model_name == "Pythia1b":
-        model = LanguageModel("EleutherAI/pythia-70m", device_map="cpu")
+        model = LanguageModel("EleutherAI/pythia-70m", device_map=device)
     elif model_name == "Pythia1.4b":
-        model = LanguageModel("EleutherAI/pythia-70m", device_map="cpu")
+        model = LanguageModel("EleutherAI/pythia-70m", device_map=device)
     elif model_name == "Pythia2.8b":
-        model = LanguageModel("EleutherAI/pythia-70m", device_map="cpu")
+        model = LanguageModel("EleutherAI/pythia-70m", device_map=device)
     elif model_name == "Pythia6.9b":
-        model = LanguageModel("EleutherAI/pythia-70m", device_map="cpu")
+        model = LanguageModel("EleutherAI/pythia-70m", device_map=device)
     elif model_name == "Pythia12b":
-        model = LanguageModel("EleutherAI/pythia-70m", device_map="cpu")
+        model = LanguageModel("EleutherAI/pythia-70m", device_map=device)
+    
+    return model
 
-        
 
 if __name__ == "__main__":
     
@@ -37,7 +38,16 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     
     parser.add_argument("--model_name", type = str, default="Pythia70m")
-    parser.add_argument("--batch_size", type=int, default="8")
+    parser.add_argument("--batch_size", type=int, default="16")
+    parser.add_argument("--data", type = str, default = "tinystories")
+    
+    args = parser.parse_args()
+    
+    model_name = args.model_name
+    batch_size = args.batch_size
+    dataset_used = args.data
+    
+    
     
     
     
---
+++ setup.sh
@@ -0,0 +1,8 @@
+pip install nnsight
+pip install jaxtyping
+pip install datasets
+pip install wandb
+
+pip install -r requirements.txt
+
+mkdir data
