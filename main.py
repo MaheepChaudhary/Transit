1470e34589d9de790bc26c951990baabce10444e
MaheepChaudhary <chaudhary.maheep28@gmail.com>
2024-09-19 21:51:01 +0530

    model selection

--- main.py
+++ main.py
@@ -1,7 +1,43 @@
 from imports import *
 from dataset import *
 
+def model_selection(model_name):
+    if model_name == "Pythia14m":
+        model = LanguageModel("EleutherAI/pythia-14m", device_map="cpu")
+    elif model_name == "Pythia70m":
+        model = LanguageModel("EleutherAI/pythia-70m", device_map="cpu")
+    elif model_name == "Pythia160m":
+        model = LanguageModel("EleutherAI/pythia-70m", device_map="cpu")
+    elif model_name == "Pythia410m":
+        model = LanguageModel("EleutherAI/pythia-70m", device_map="cpu")
+    elif model_name == "Pythia1b":
+        model = LanguageModel("EleutherAI/pythia-70m", device_map="cpu")
+    elif model_name == "Pythia1.4b":
+        model = LanguageModel("EleutherAI/pythia-70m", device_map="cpu")
+    elif model_name == "Pythia2.8b":
+        model = LanguageModel("EleutherAI/pythia-70m", device_map="cpu")
+    elif model_name == "Pythia6.9b":
+        model = LanguageModel("EleutherAI/pythia-70m", device_map="cpu")
+    elif model_name == "Pythia12b":
+        model = LanguageModel("EleutherAI/pythia-70m", device_map="cpu")
+
+        
+
 if __name__ == "__main__":
     
-    pass
+    #TODO: 
+    '''
+    1. Write the code to setup Pythia. 
+    2. Setup the code to load all the datasets. 
+    3. Write the code to get the activations and save it in the figure. 
+    4. Write the code to get the activations and save it in the figure. 
+    5. Write the code to run the code for all the selected models. 
+    '''
+    
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument("--model_name", type = str, default="Pythia70m")
+    parser.add_argument("--batch_size", type=int, default="8")
+    
+    
     
