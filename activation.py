f807c93f5dc81eb8e8f60997bc876663f239b6fc
MaheepChaudhary <chaudhary.maheep28@gmail.com>
2024-09-20 11:31:11 +0530

    figures produced

--- activation.py
+++ activation.py
@@ -231,9 +231,9 @@ class act_pythia_mlp:
         if self.dataset_name == "tinystories":
             self.max_length = 145
         elif self.dataset_name == "summarisation":
-            self.max_length = 1500
+            self.max_length = 340
         elif self.dataset_name == "alpaca":
-            self.max_length = 50
+            self.max_length = 10
         
         
     def activation_embeds_fn(self): # So it contains 5 layers and one last layer. 
--- run.sh
+++ run.sh
@@ -6,12 +6,12 @@
 # python main.py --batch_size 16 --model_name "Pythia1.4b" --data "tinystories" --device "mps"
 
 
-# python main.py --batch_size 16 --model_name "Pythia14m" --data "alpaca" --device "mps"
-# python main.py --batch_size 16 --model_name "Pythia70m" --data "alpaca" --device "mps"
-# python main.py --batch_size 16 --model_name "Pythia160m" --data "alpaca" --device "mps"
-# python main.py --batch_size 16 --model_name "Pythia410m" --data "alpaca" --device "mps"
-# python main.py --batch_size 16 --model_name "Pythia1b" --data "alpaca" --device "mps"
-# python main.py --batch_size 16 --model_name "Pythia1.4b" --data "alpaca" --device "mps"
+python main.py --batch_size 16 --model_name "Pythia14m" --data "alpaca" --device "mps"
+python main.py --batch_size 16 --model_name "Pythia70m" --data "alpaca" --device "mps"
+python main.py --batch_size 16 --model_name "Pythia160m" --data "alpaca" --device "mps"
+python main.py --batch_size 16 --model_name "Pythia410m" --data "alpaca" --device "mps"
+python main.py --batch_size 16 --model_name "Pythia1b" --data "alpaca" --device "mps"
+python main.py --batch_size 16 --model_name "Pythia1.4b" --data "alpaca" --device "mps"
 
 
 python main.py --batch_size 16 --model_name "Pythia14m" --data "summarisation" --device "mps"
