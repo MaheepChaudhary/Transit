ac9919350b3565a61dc931c927f46085dc6eefd0
MaheepChaudhary <chaudhary.maheep28@gmail.com>
2024-09-23 07:46:21 +0530

    gradient figures.

--- cuda_run.sh
+++ cuda_run.sh
@@ -1,20 +1,20 @@
-# python main.py --batch_size 16 --model_name "Pythia14m" --data "tinystories" --device "cuda"
-python main.py --batch_size 16 --model_name "Pythia70m" --data "tinystories" --device "cuda"
+python main.py --batch_size 16 --model_name "Pythia14m" --data "tinystories" --device "cuda"
+# python main.py --batch_size 16 --model_name "Pythia70m" --data "tinystories" --device "cuda"
 python main.py --batch_size 16 --model_name "Pythia160m" --data "tinystories" --device "cuda"
 python main.py --batch_size 16 --model_name "Pythia410m" --data "tinystories" --device "cuda"
 python main.py --batch_size 16 --model_name "Pythia1b" --data "tinystories" --device "cuda"
 python main.py --batch_size 16 --model_name "Pythia1.4b" --data "tinystories" --device "cuda"
 
 
-python main.py --batch_size 16 --model_name "Pythia14m" --data "alpaca" --device "cuda"
-python main.py --batch_size 16 --model_name "Pythia70m" --data "alpaca" --device "cuda"
-python main.py --batch_size 16 --model_name "Pythia160m" --data "alpaca" --device "cuda"
-python main.py --batch_size 16 --model_name "Pythia410m" --data "alpaca" --device "cuda"
+# python main.py --batch_size 16 --model_name "Pythia14m" --data "alpaca" --device "cuda"
+# python main.py --batch_size 16 --model_name "Pythia70m" --data "alpaca" --device "cuda"
+# python main.py --batch_size 16 --model_name "Pythia160m" --data "alpaca" --device "cuda"
+# python main.py --batch_size 16 --model_name "Pythia410m" --data "alpaca" --device "cuda"
 python main.py --batch_size 16 --model_name "Pythia1b" --data "alpaca" --device "cuda"
 python main.py --batch_size 16 --model_name "Pythia1.4b" --data "alpaca" --device "cuda"
 
 
-python main.py --batch_size 16 --model_name "Pythia14m" --data "summarisation" --device "cuda"
+# python main.py --batch_size 16 --model_name "Pythia14m" --data "summarisation" --device "cuda"
 python main.py --batch_size 16 --model_name "Pythia70m" --data "summarisation" --device "cuda"
 python main.py --batch_size 16 --model_name "Pythia160m" --data "summarisation" --device "cuda"
 python main.py --batch_size 16 --model_name "Pythia410m" --data "summarisation" --device "cuda" 
