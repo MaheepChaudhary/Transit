6f8b9dd285f0a6341959fb8bb2ad9879fc53576c
MaheepChaudhary <chaudhary.maheep28@gmail.com>
2024-09-24 09:10:14 +0530

    gradient customized for each model.

--- cuda_run.sh
+++ cuda_run.sh
@@ -1,11 +1,11 @@
-echo "Running Pythia14m with tinystories"
-python main.py --batch_size 16 --model_name "Pythia14m" --data "tinystories" --device "cuda"
+# echo "Running Pythia14m with tinystories"
+# python main.py --batch_size 16 --model_name "Pythia14m" --data "tinystories" --device "cuda"
 
-echo "Running Pythia70m with tinystories"
-python main.py --batch_size 16 --model_name "Pythia70m" --data "tinystories" --device "cuda"
+# echo "Running Pythia70m with tinystories"
+# python main.py --batch_size 16 --model_name "Pythia70m" --data "tinystories" --device "cuda"
 
-echo "Running Pythia160m with tinystories"
-python main.py --batch_size 16 --model_name "Pythia160m" --data "tinystories" --device "cuda"
+# echo "Running Pythia160m with tinystories"
+# python main.py --batch_size 16 --model_name "Pythia160m" --data "tinystories" --device "cuda"
 
 echo "Running Pythia410m with tinystories"
 python main.py --batch_size 16 --model_name "Pythia410m" --data "tinystories" --device "cuda"
@@ -17,23 +17,23 @@ echo "Running Pythia1.4b with tinystories"
 python main.py --batch_size 16 --model_name "Pythia1.4b" --data "tinystories" --device "cuda"
 
 
-echo "Running Pythia14m with alpaca"
-python main.py --batch_size 16 --model_name "Pythia14m" --data "alpaca" --device "cuda"
+# echo "Running Pythia14m with alpaca"
+# python main.py --batch_size 16 --model_name "Pythia14m" --data "alpaca" --device "cuda"
 
-echo "Running Pythia70m with alpaca"
-python main.py --batch_size 16 --model_name "Pythia70m" --data "alpaca" --device "cuda"
+# echo "Running Pythia70m with alpaca"
+# python main.py --batch_size 16 --model_name "Pythia70m" --data "alpaca" --device "cuda"
 
-echo "Running Pythia160m with alpaca"
-python main.py --batch_size 16 --model_name "Pythia160m" --data "alpaca" --device "cuda"
+# echo "Running Pythia160m with alpaca"
+# python main.py --batch_size 16 --model_name "Pythia160m" --data "alpaca" --device "cuda"
 
-echo "Running Pythia410m with alpaca"
-python main.py --batch_size 16 --model_name "Pythia410m" --data "alpaca" --device "cuda"
+# echo "Running Pythia410m with alpaca"
+# python main.py --batch_size 16 --model_name "Pythia410m" --data "alpaca" --device "cuda"
 
-echo "Running Pythia1b with alpaca"
-python main.py --batch_size 16 --model_name "Pythia1b" --data "alpaca" --device "cuda"
+# echo "Running Pythia1b with alpaca"
+# python main.py --batch_size 16 --model_name "Pythia1b" --data "alpaca" --device "cuda"
 
-echo "Running Pythia1.4b with alpaca"
-python main.py --batch_size 16 --model_name "Pythia1.4b" --data "alpaca" --device "cuda"
+# echo "Running Pythia1.4b with alpaca"
+# python main.py --batch_size 16 --model_name "Pythia1.4b" --data "alpaca" --device "cuda"
 
 
 echo "Running Pythia14m with summarisation"
