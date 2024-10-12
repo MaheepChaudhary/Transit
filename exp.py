9e5c9fbdb3be0309dd25e83e7a4605855cabed97
MaheepChaudhary <chaudhary.maheep28@gmail.com>
2024-10-12 20:26:48 +0530

    updated

--- exp.py
+++ exp.py
@@ -1,37 +1,15 @@
-from imports import *
+from nnsight import LanguageModel
 
-pythia14m = LanguageModel("EleutherAI/pythia-14m", device_map="cuda")
-pythia70m = LanguageModel("EleutherAI/pythia-70m", device_map="cuda")
-pythia160m = LanguageModel("EleutherAI/pythia-160m", device_map="cuda")
-pythia410m = LanguageModel("EleutherAI/pythia-410m", device_map="cuda")
-pythia1b = LanguageModel("EleutherAI/pythia-1b", device_map="cuda")
-pythia1_4b = LanguageModel("EleutherAI/pythia-1.4b", device_map="cuda")
+model = LanguageModel("EleutherAI/pythia-70m", device_map = "cpu")
 
-print("Printing pythia 14m model")
-print(pythia14m)
+input_text = 'One day, a dog was roaming'
 
-print()
+tokenizer = AutoTokenizer.from_pretrained('EleutherAI/pythia-70m')
 
-print("Printing pythia 70m model")
-print(pythia70m)
+# inputs = tokenizer(input_text, return_tensors="pt", padding='max_length', max_length=128, truncation=True)
+print(model)
 
-print()
+with model.trace(input_text) as tracer:
+    output = model.embed_out.output.save()
 
-print("Printing pythia 160m model")
-print(pythia160m)
-
-print()
-
-print("Printing pythia 410m model")
-print(pythia410m)
-
-print()
-
-print("Printing pythia 1b model")
-
-print(pythia1b)
-print()
-
-print("Printing pythia 1.4b model")
-print(pythia1_4b)
-print()
+print(output)
