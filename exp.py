6460c1a0c0cf1362c90324048a2af6b5e89adc34
MaheepChaudhary <chaudhary.maheep28@gmail.com>
2024-10-18 12:23:00 +0530

    just analysing after concatenating images.

--- exp.py
+++ exp.py
@@ -1,15 +1,16 @@
 from nnsight import LanguageModel
 
 model = LanguageModel("EleutherAI/pythia-70m", device_map = "cpu")
-
 input_text = 'One day, a dog was roaming'
-
-tokenizer = AutoTokenizer.from_pretrained('EleutherAI/pythia-70m')
 
 # inputs = tokenizer(input_text, return_tensors="pt", padding='max_length', max_length=128, truncation=True)
 print(model)
 
 with model.trace(input_text) as tracer:
+    l_out = model.gpt_neox.layers[0].attention.query_key_value.output.save()
     output = model.embed_out.output.save()
 
-print(output)
+
+print(l_out)
+
+
